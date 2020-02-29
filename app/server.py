80296b765a8620048b92123b62a5dfb00206a79b
hansvankeulen <40041419+hansvankeulen@users.noreply.github.com>
2020-02-29 17:03:03 +0100

    Change to suit my tree detector
    Point to the model file to download and edited the categories.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/5ofvm02hqn53a42/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['esdoorn', 'haagbeuk', 'kastanje', 'beuk', 'populier', 'eik']
 path = Path(__file__).parent
 
 app = Starlette()
